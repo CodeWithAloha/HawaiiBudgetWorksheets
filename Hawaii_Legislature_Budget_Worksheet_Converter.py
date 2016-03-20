082b92614f4fb0122c46c0f21b4335a489fa5414
McKay Davis <mckay.davis@gmail.com>
2016-03-20 11:13:13 -1000

    Create HBWSPage class to parse a single budget worksheet page of text.  Correctly parses header line for datetime, page number, and total # of pages.

--- Hawaii_Legislature_Budget_Worksheet_Converter.py
+++ Hawaii_Legislature_Budget_Worksheet_Converter.py
@@ -16,11 +16,55 @@ __email__ = "mckay@codeforhawaii.org"
 import subprocess
 import sys
 import getopt
-
+import datetime
+import re
 
 
 def main():
     text = pdftotext(sys.argv[1])
+    textpages = text.split("\x0c")
+    pages = [HBWSPage(pagetext) for pagetext in textpages[:-1]]
+    [page.debug_print() for page in pages]
+
+
+
+class HBWSPage:
+    """Hawaii Budget Worksheet Page"""
+    def __init__(self, text):
+        self.lines = text.split("\n")
+        self.parse()
+
+    def parse_timestamp(self, line):
+        # insert leading 0 for hour in time
+        if line[2][1] == ":": line[2] = "0" + line[2]
+        timestring = " ".join(line[1:3])
+        timeformat = "%A, %B %d, %Y %H:%M:%S %p"
+        return datetime.datetime.strptime(timestring, timeformat)
+
+    def parse_pagenum(self, text):
+        assert text[:5] == "Page "
+        components = text[5:].split(" of ")
+        assert len(components) == 2, "pagenum parsing failed to find ' of '"
+        return (int(components[0]), int(components[1]))
+
+
+    def parse_line0(self):
+        line0 = re.split(" \s+", self.lines[0])
+        assert line0[3] == "LEGISLATIVE BUDGET SYSTEM", "line0 is not header "+"\t".join(line0)
+        self.datetime = self.parse_timestamp(line0)
+        self.pagenum,self.pages = self.parse_pagenum(line0[-1])
+
+    def parse_header(self):
+        self.parse_line0()
+
+
+    def parse(self):
+        self.parse_header()
+
+    def debug_print(self):
+        print("datetime", self.datetime)
+        print("page num", self.pagenum)
+        print("pages", self.pages)
 
 
 def pdftotext(pdf_filename):
@@ -29,8 +73,9 @@ def pdftotext(pdf_filename):
            "-fixed 3",
            pdf_filename,
            "-"]
-    output = subprocess.check_output(" ".join(cmd), shell=True)
-    return output
+    buf = subprocess.check_output(" ".join(cmd), shell=True)
+    text = buf.decode("utf-8")
+    return text
 
 
 if __name__ == "__main__":
