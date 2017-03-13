109ac6cecf6ccb7cc967707949c63aaa29cfbfcd
McKay H Davis <mckay.davis@gmail.com>
2017-03-13 11:49:13 -1000

    Spans.py class for deducing columns of text

---
+++ bin/Spans.py
@@ -0,0 +1,94 @@
+__author__ = "McKay H Davis"
+__date__ = "2017-03-13Z11:47"
+__copyright__ = "Copyright 2017"
+__license__ = "GPLv3"
+__maintainer__ = "McKay Davis"
+__email__ = "mckay@codeforhawaii.org"
+__doc__ = """
+Spans.py:
+Python class to handle 1D spans, used to deduce columns in text output from pdftotext
+"""
+
+class Spans(object):
+    neg_inf = float('-inf')
+    pos_inf = float('inf')
+    range_all = (neg_inf, pos_inf)
+    range_none = (pos_inf, neg_inf)
+    def __init__(self, a = None, b = None, quant = None):
+        a = self.pos_inf if a is None else a
+        b = self.neg_inf if b is None else b
+
+        if quant:
+            a = int(math.floor(a/quant)+0.1)
+            b = int(math.floor(b/quant)+0.1)
+
+        self.ss = [(a,b)]
+
+    def __str__(self):
+        return "{}".format(self.ss)
+
+    @staticmethod
+    def _combine(*args):
+        result = []
+
+        for sss in args:
+            result += [(s[0], 0) for s in sss.ss]
+            result += [(s[1], 1) for s in sss.ss]
+
+        result = list(sorted(result))
+        return result
+
+    def union(self, them):
+        both = []
+        count = 0
+        for s in Spans._combine(self, them):
+            if s[1] == 0:
+                count += 1
+                if count == 1:
+                    left = s[0]
+            else:
+                count -= 1
+                if count == 0:
+                    both.append((left, s[0]))
+
+        res = Spans()
+        res.ss = both
+
+        return res
+
+    def intersect(self, them):
+        both = []
+        count = 0
+        for s in Spans._combine(self,them):
+            if s[1] == 0:
+                count += 1
+                if count == 2:
+                    left = s[0]
+            else:
+                count -= 1
+                if count == 1:
+                    both.append((left, s[0]))
+
+        res = Spans()
+        res.ss = both
+
+        return res
+
+    def index(self, span):
+        i = 0
+        while i < len(self.ss) and span[0] > self.ss[i][1]:
+            i += 1
+        if i < len(self.ss) and span[0] <= self.ss[i][1]:
+            assert span[0] >= self.ss[i][0], "span={} ss[{}]={}".format(span, i, self.ss[i])
+            assert span[1] <= self.ss[i][1], "span={} ss[{}]={}".format(span, i, self.ss[i])
+            return i
+        return -1
+
+
+    @staticmethod
+    def from_text(text):
+        spans = Spans()
+        char_spans = [Spans(i, i+1) for i, char in enumerate(text) if char != " "]
+        for char_span in char_spans:
+            spans = spans.union(char_span)
+        return spans
